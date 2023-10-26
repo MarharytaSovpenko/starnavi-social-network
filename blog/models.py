90b6ee271e0ba2e940062f1ca0e5e9da038ded05
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-10-26 16:40:34 +0300

    Create post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+def get_anonymous_user():
+    return get_user_model().objects.get_or_create(
+        gmail="anonymous", password="anonymous"
+    )[0]
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET(get_anonymous_user),
+        related_name="authored_posts",
+    )
+    title = models.CharField(max_length=125, unique=True)
+    likes = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, blank=True, related_name="liked_posts"
+    )
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
